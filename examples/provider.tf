a5c179e9a3176e9732cde2b597ff23d03be9a078
Majid S <m.sotoodeh@gmail.com>
2022-03-02 17:35:44 -0800

    update

--- examples/provider.tf
+++ examples/provider.tf
@@ -1,15 +1,14 @@
 terraform {
-  
+  # Terraform Engine version
+  required_version = ">=0.15.1"
   required_providers {
     mso = {
       # MSO Provider Source
       source = "CiscoDevNet/mso"
       # MSO Provider Version
-     
+      version = ">=0.1.5"
     }
   }
-  # Terraform Engine version
-  required_version = ">=0.15.1"
 }
 
 provider "mso" {
