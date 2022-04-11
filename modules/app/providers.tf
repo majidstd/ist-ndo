28cf5e7c382031eae777a601e8f9be2483abe46e
Majid S <m.sotoodeh@gmail.com>
2022-04-11 13:00:38 -0700

    Update providers.tf

--- modules/app/providers.tf
+++ modules/app/providers.tf
@@ -1,8 +1,8 @@
-terraform {
-  required_providers {
-    mso = {
-      # MSO Provider Source
-      source = "CiscoDevNet/mso"
-    }
-  }
-}
+# terraform {
+  # required_providers {
+    # mso = {
+      MSO Provider Source
+      # source = "CiscoDevNet/mso"
+    # }
+  # }
+# }
